6f05171576183284daa2b343a622d781941bcd8b
johanaalfonso809 <johanaalfonso809@gmail.com>
2025-12-05 00:01:55 +0100

    matriz terminada

---
+++ basico.cpp
@@ -0,0 +1,25 @@
+//ejercio de condicion
+/*#include <iostream>
+using namespace std;
+
+int main(){
+    int edad;
+    cout<<"ingrese su edad; ";
+    cin>>edad;
+
+    if (edad>=18){
+        cout<<"eres mayor";
+    }
+    else { 
+        cout<<"eres menor";
+    }
+
+    return 0;
+} */
+
+
+
+
+
+
+
